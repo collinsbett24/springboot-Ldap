3266c9371041b438a41e3440d1ae1b22a94e3781
Collins Bett <collinsbett24@gmail.com>
2024-02-04 09:24:09 +0300

    First commit

---
+++ src/main/java/com/blins/springbootldap/SpringbootLdapApplication.java
@@ -0,0 +1,14 @@
+package com.blins.springbootldap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootLdapApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		SpringApplication.run(SpringbootLdapApplication.class, args);
+	}
+
+}
