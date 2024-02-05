92c69668e2eb51d004d28b8cb30f1fbcfc20eb2d
Collins Bett <collinsbett24@gmail.com>
2024-02-05 21:30:57 +0300

    made changes to ldap port on app properties

--- src/main/java/com/blins/springbootldap/SpringbootLdapApplication.java
+++ src/main/java/com/blins/springbootldap/SpringbootLdapApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringbootLdapApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
 		SpringApplication.run(SpringbootLdapApplication.class, args);
 	}
 
