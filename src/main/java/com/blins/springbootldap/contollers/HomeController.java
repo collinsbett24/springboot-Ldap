331c926f26f798f2dbc3aa4ce84243b61769abfe
Collins Bett <collinsbett24@gmail.com>
2024-02-05 21:18:31 +0300

    added an test rest controller

--- src/main/java/com/blins/springbootldap/contollers/HomeController.java
+++ src/main/java/com/blins/springbootldap/contollers/HomeController.java
@@ -1,4 +1,14 @@
 package com.blins.springbootldap.contollers;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class HomeController {
+    @GetMapping("home")
+    public  String index(){
+        return "Welcome Home";
+    }
 }
